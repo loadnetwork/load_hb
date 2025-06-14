f1ccfc1f11f78651c846c4d31ee3d639e16a1915
charmful0x <charmful0x@gmail.com>
2025-06-14 16:58:21 +0200

    chore: remove unnecessary ao test

--- src/dev_roam.erl
+++ src/dev_roam.erl
@@ -2,7 +2,7 @@
 %%% built on top of ureq HTTP client, and goldsky GQL gateway the roam@1.0 device provides
 
 -module(dev_roam).
--export([info/1, info/3, query_permaweb/3, hello/3, test_ao/0]).
+-export([info/1, info/3, query_permaweb/3, hello/3]).
 
 info(_) ->
     #{
@@ -64,55 +64,5 @@ query_permaweb(_Msg1, _Msg2, _Opts) ->
     end.
 
 %  example calls:
-% curl -X GET http://localhost:10001/~roam@1.0/hello
-% curl -X GET http://localhost:10001/~roam@1.0/query_permaweb 
-
-% in erlang shell call: dev_roam:test_ao().
-test_ao() ->
-    io:format("~n__test_roam_device__~n"),
-    try
-        % Get current wallet
-        Wallet = hb:wallet(),
-        % Get wallet address in human readable form  
-        Address = hb_util:human_id(ar_wallet:to_address(Wallet)),
-
-        % Create the process message
-        {ok, Script} = file:read_file("test/roam-device.lua"),
-        Process = hb_message:commit(#{
-            <<"device">> => <<"process@1.0">>,
-            <<"type">> => <<"Process">>,
-            <<"scheduler-device">> => <<"scheduler@1.0">>,
-            <<"execution-device">> => <<"lua@5.3a">>,
-            <<"script">> => Script,
-            <<"scheduler-location">> => Address,
-            <<"authority">> => [Address],  % Add authority
-            <<"test-random-seed">> => rand:uniform(1337)
-        }, Wallet),
-
-        % Cache the process
-        {ok, _} = hb_cache:write(Process, #{}),
-
-        % Get process ID
-        ProcID = hb_message:id(Process, all),
-
-        % Create schedule message  
-        Message = hb_message:commit(#{
-            <<"path">> => <<"schedule">>,
-            <<"method">> => <<"POST">>,
-            <<"body">> => hb_message:commit(#{
-                <<"target">> => ProcID,
-                <<"type">> => <<"Message">>,
-                <<"action">> => <<"Eval">>
-            }, Wallet)
-        }, Wallet),
-
-        % Schedule the message
-        {ok, _} = hb_ao:resolve(Process, Message, #{}),
-
-        % Get the results
-        {ok, Results} = hb_ao:resolve(Process, <<"now">>, #{}),
-        io:format("~nRoam test result: ~p~n", [Results])
-    catch
-        Error:Reason:Stack ->
-            io:format("Error running Roam test: ~p:~p~n~p~n", [Error, Reason, Stack])
-    end.
+% curl -X GET http://localhost:8734/~roam@1.0/hello
+% curl -X GET http://localhost:8734/~roam@1.0/query_permaweb 
