97293833b8c4b9e3a420fd6840407551915920bf
charmful0x <charmful0x@gmail.com>
2025-06-28 14:33:43 +0200

    feat: add rustler

--- native/quantum_runtime/src/lib.rs
+++ native/quantum_runtime/src/lib.rs
@@ -1,9 +1,14 @@
-use roqoqo_quest::Backend;
 pub mod core;
 pub mod tests;
+use rustler::NifResult;
 
-fn main() {
-    let backend = Backend::new(2, None);
-    println!("{:?}", backend);
-    println!("roqoqo-quest installed successfully!");
+mod atoms {
+    rustler::atoms! {
+        ok,
+    }
+}
+
+#[rustler::nif]
+fn hello() -> NifResult<String> {
+    Ok("Hello world!".to_string())
 }
