78a5b095fc39283106df16fe4977564b97a2ec62
charmful0x <charmful0x@gmail.com>
2025-07-12 19:13:03 +0200

    feat: nif create_bucket fn

--- native/s3_nif/src/s3/create_bucket.rs
+++ native/s3_nif/src/s3/create_bucket.rs
@@ -1,7 +1,10 @@
-use aws_sdk_s3::Client;
+use aws_sdk_s3::{Client, operation::create_bucket::CreateBucketOutput};
 
-pub async fn create_bucket(client: &Client, bucket_name: &str) -> Result<(), aws_sdk_s3::Error> {
-    client.create_bucket().bucket(bucket_name).send().await?;
+pub async fn create_bucket(
+    client: &Client,
+    bucket_name: &str,
+) -> Result<CreateBucketOutput, aws_sdk_s3::Error> {
+    let res: CreateBucketOutput = client.create_bucket().bucket(bucket_name).send().await?;
 
-    Ok(())
+    Ok(res)
 }
