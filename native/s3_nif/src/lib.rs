efffe9a62f3ea87dfb1726b52a728298ce2c48a9
charmful0x <charmful0x@gmail.com>
2025-07-13 19:04:07 +0200

    feat: head_object nif method

--- native/s3_nif/src/lib.rs
+++ native/s3_nif/src/lib.rs
@@ -2,6 +2,9 @@ pub mod config;
 pub mod nif_methods;
 pub mod s3;
 pub mod server;
-use crate::nif_methods::{create_bucket, get_object, put_object};
+use crate::nif_methods::{create_bucket, get_object, head_object, put_object};
 
-rustler::init!("s3_nif", [get_object, put_object, create_bucket]);
+rustler::init!(
+    "s3_nif",
+    [get_object, put_object, create_bucket, head_object]
+);
--- native/s3_nif/src/nif_methods/mod.rs
+++ native/s3_nif/src/nif_methods/mod.rs
@@ -1,7 +1,9 @@
 mod create_bucket_nif;
 mod get_object_nif;
+mod head_object_nif;
 mod put_object_nif;
 
 pub use create_bucket_nif::create_bucket;
 pub use get_object_nif::get_object;
+pub use head_object_nif::head_object;
 pub use put_object_nif::put_object;
